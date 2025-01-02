80e0a04e57fe0050393bf5775406edc3964129c0
Joshua Coles <me@joshuacoles.me>
2025-01-02 12:44:56 +0000

    Initial work

--- main.ts
+++ main.ts
@@ -3,83 +3,68 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	hideByDefault: boolean;
+	pathExceptions: string[];
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	hideByDefault: false,
+	pathExceptions: []
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	private styleEl: HTMLStyleElement;
+	private isEnabled: boolean = true;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Create stylesheet element
+		this.styleEl = document.createElement('style');
+		this.styleEl.id = 'file-tree-focus-styles';
+		document.head.appendChild(this.styleEl);
+		this.updateStyles();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'toggle-file-tree-focus',
+			name: 'Toggle file tree focus',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.isEnabled = !this.isEnabled;
+				this.updateStyles();
+				new Notice(`${this.isEnabled ? 'Enabled' : 'Disabled'} file tree focus`);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
+		// Remove the stylesheet when plugin is disabled
+		if (this.styleEl && this.styleEl.parentNode) {
+			this.styleEl.parentNode.removeChild(this.styleEl);
+		}
+	}
 
+	public updateStyles() {
+		if (!this.styleEl) return;
+
+		if (this.isEnabled) {
+			const css = this.settings.hideByDefault
+				? `
+					.tree-item { display: none }
+					${this.settings.pathExceptions.map(path => 
+						`.tree-item:has(> .tree-item-self[data-path^="${path}"]) { display: block }`
+					).join('\n')}
+				`
+				: this.settings.pathExceptions.map(path => 
+					`.tree-item:has(> .tree-item-self[data-path^="${path}"]) { display: none }`
+				).join('\n');
+			
+			this.styleEl.textContent = css;
+		} else {
+			this.styleEl.textContent = '';
+		}
 	}
 
 	async loadSettings() {
@@ -117,18 +102,32 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Hide by default')
+			.setDesc('If enabled, hides all folders except those listed below. If disabled, shows all folders except those listed below.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.hideByDefault)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.hideByDefault = value;
 					await this.plugin.saveSettings();
+					this.plugin.updateStyles();
+				}));
+
+		new Setting(containerEl)
+			.setName('Path Exceptions')
+			.setDesc('List of paths that are exceptions to the default behavior')
+			.addTextArea(text => text
+				.setPlaceholder('One path per line')
+				.setValue(this.plugin.settings.pathExceptions.join('\n'))
+				.onChange(async (value) => {
+					this.plugin.settings.pathExceptions = value
+						.split('\n')
+						.map(p => p.trim())
+						.filter(p => p.length > 0);
+					await this.plugin.saveSettings();
+					this.plugin.updateStyles();
 				}));
 	}
 }
